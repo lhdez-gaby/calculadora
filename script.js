bec5839c72895df80c122c579e50362ce7ee023e
Gabriela López <lhdez.gaby@gmail.com>
2023-10-27 18:00:31 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Agrega el valor del botón a la pantalla
+
+const add = (valor) => {
+    document.getElementById('screen').value += valor
+}
+
+// function add (value){
+//     document.getElementById('screen').value += value
+// }
